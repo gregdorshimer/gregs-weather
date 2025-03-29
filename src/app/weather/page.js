6ac6727f818031bec7c51fc277258803aa8d2845
Greg Dorshimer <greg.dorshimer@protonmail.com>
2025-03-29 15:54:36 -0400

    next.js patch

--- src/app/weather/page.js
+++ src/app/weather/page.js
@@ -1,60 +1,30 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Chau_Philomene_One } from "next/font/google";
 import Link from "next/link"
-import Suspense from 'react'
 import WeatherContent from "./weather-content";
 
-
-async function fetchForecast() {
-    // NWS API docs:
-    // Docs home: https://www.weather.gov/documentation/services-web-api
-    // FAQ: https://weather-gov.github.io/api/gridpoints
-
-    // Find coordinates on the map: https://gps-coordinates.org/
-
-    // Find forecast office codes: https://en.wikipedia.org/wiki/List_of_National_Weather_Service_Weather_Forecast_Offices
-    
-    // Get a grid loc (properties.gridX, and properties.gridY, and office is properties.gridId) given
-    // coordinates:
-    // https://api.weather.gov/points/43.66%2C-70.26
-    // return GYX, 76, 59 (Portland, ME), among other data
-
-    // Get forecast for an office and a grid loc: https://api.weather.gov/gridpoints/GYX/31,80/forecast
-
-    // TODO
-
-    const data = await fetch("https://api.weather.gov/gridpoints/GYX/31,80/forecast");
-    const forecast = await data.json();
-    console.log(forecast.properties);
-    return forecast.properties;
-}
-
 export default function WeatherPage({children}) {
-    // const promise = fetchForecast();
-    // const promise = fetch("https://api.weather.gov/gridpoints/GYX/31,80/forecast");
 
     return (
-        // <Suspense fallback={<div>Loading...</div>}>
-            <div className="container text-center">
-                            
-                <div className="row">
-                    {/* TODO title area */}
-                    <h1>Weather</h1>
-                    <Link href="/"><button type="button" className="btn btn-primary">Home</button></Link>
-                </div>
-            
-
-            
-                    <WeatherContent />
-            
-            
-            
-                <div className="container text-center fixed-bottom">
-                    <p>Developed by Greg Dorshimer • March 2025</p>
-                    {/* TODO footer here e.g. "Built by Greg Dorshimer 2025" */}
-                </div>
-        
+        <div className="container text-center">
+                        
+            <div className="row">
+                {/* TODO title area */}
+                <h1>Weather</h1>
+                <Link href="/"><button type="button" className="btn btn-primary">Home</button></Link>
             </div>
-        // </Suspense>
+        
+
+        
+                <WeatherContent />
+        
+        
+        
+            <div className="container text-center fixed-bottom">
+                <p>Developed by Greg Dorshimer • March 2025</p>
+                {/* TODO footer style */}
+            </div>
+    
+        </div>
     );
 }
--- src/app/weather/weather-content.js
+++ src/app/weather/weather-content.js
@@ -1,37 +1,94 @@
 "use client";
-import { useState, use } from "react";
+import { useState, useEffect } from "react";
+import useSWR from "swr";
 
-function Forecast({data}) {
-    return (
-        <>
-            <p>{data}</p>
-        </>
+
+const testURL1 = "https://api.weather.gov/gridpoints/GYX/31,80/forecast";
+const testURL2 = "https://api.weather.gov/gridpoints/GYX/76,59/forecast";
+
+async function fetchForecast(resource) {
+    // NWS API docs:
+    // Docs home: https://www.weather.gov/documentation/services-web-api
+    // FAQ: https://weather-gov.github.io/api/gridpoints
+
+    // Find coordinates on the map: https://gps-coordinates.org/
+
+    // Find forecast office codes: https://en.wikipedia.org/wiki/List_of_National_Weather_Service_Weather_Forecast_Offices
+    
+    // Get a grid loc (properties.gridX, and properties.gridY, and office is properties.gridId) given
+    // coordinates:
+    // https://api.weather.gov/points/43.66%2C-70.26
+    // return GYX, 76, 59 (Portland, ME), among other data
+
+    // Get forecast for an office and a grid loc: https://api.weather.gov/gridpoints/GYX/31,80/forecast
+    // https://api.weather.gov/gridpoints/GYX/76,59/forecast
+
+    // TODO take params to construct URL
+
+    const fetcher = (url) => fetch(url).then((r) => r.json());
+    
+    const { data, error, isLoading } = useSWR(
+        resource,
+        fetcher
     );
+
+    if (isLoading) return <p>Loading...</p>;
+
+    if (error) return <p>Failed to fetch data.</p>;
+
+    return data;
+
+    // const data = await fetch("https://api.weather.gov/gridpoints/GYX/31,80/forecast");
+    // const forecast = await data.json();
+    // console.log(forecast.properties);
+    // return forecast.properties;
 }
 
-function ForecastButton({counter, onClick}) {
-    
+function Forecast({setForecast, resource}) {
+    const fetcher = (url) => fetch(url).then((r) => r.json());
+
+    const { data, error, isLoading } = useSWR(
+        resource,
+        fetcher
+    );
+
+    if (isLoading) { setForecast("Loading..."); return <></>; }
+
+    if (error) { setForecast("Failed to fetch data."); return <></>; }
+
+    setForecast(data);
+
+    return <>{data}</>;
+}
+
+function ForecastButton({setForecast, setStartFetching, resource}) {
+
     function handleClick() {
-        console.log("fetch forecast clicked");
-        counter++;
-        onClick(counter);
+        setStartFetching(true);
     }
 
     return (
-        <button 
-            type="button"
-            className="btn btn-primary"
-            onClick={handleClick}>
-            Fetch Forecast
-        </button>
+        <>
+            <button 
+                type="button"
+                className="btn btn-primary"
+                onClick={handleClick}>
+                Fetch Forecast
+            </button>
+        </>
     );
 
 }
 
 export default function WeatherContent({forecastPromise}) {
-    const [testForecast, setTestForecast] = useState(0);
+    // const [testForecast, setTestForecast] = useState(0);
+    const [currentLoc, setCurrentLoc] = useState(testURL1);
+    const [forecast, setForecast] = useState();
+    const [startFetching, setStartFetching] = useState(false);
 
     // const forecast = use(forecastPromise);
+    // const promise = fetchForecast();
+    // const promise = fetch("https://api.weather.gov/gridpoints/GYX/31,80/forecast");
 
     return (
         <div className="row">
@@ -46,11 +103,9 @@ export default function WeatherContent({forecastPromise}) {
                 
                 <p>Sample forecast graph.</p>
                 
-                <ForecastButton counter={testForecast} onClick={setTestForecast}/>
+                <ForecastButton setStartFetching={setStartFetching} />
                 
-                <Forecast data={testForecast} />
-
-                {/* <p>{forecast}</p> */}
+                {startFetching && <Forecast setForecast={setForecast} url={currentLoc} />}
 
             </div>
 
