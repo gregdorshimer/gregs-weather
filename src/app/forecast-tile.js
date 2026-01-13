88b16f921018629d93f4f91f253d5fbf0dd0e93d
Greg Dorshimer <greg.dorshimer@protonmail.com>
2026-01-13 12:53:14 -0500

    cleanup

--- src/app/forecast-tile.js
+++ src/app/forecast-tile.js
@@ -6,15 +6,13 @@ export default function ForecastTile({info}) {
     }
 
     else {
-        let parsed = info;
-        
         return (
             <div className="card mx-2 my-2" style={{width: "10rem"}}>
-                <img src={parsed.icon} className="card-img-top my-2" alt="loading..." />
+                <img src={info.icon} className="card-img-top my-2" alt="loading..." />
                 <div className="card-body">
-                    <h5 className="card-title">{parsed.name}</h5>
-                    <p className="card-text">{parsed.shortForecast}</p>
-                    <h6>{parsed.temperature}&#176;{parsed.temperatureUnit}</h6>
+                    <h5 className="card-title">{info.name}</h5>
+                    <p className="card-text">{info.shortForecast}</p>
+                    <h6>{info.temperature}&#176;{info.temperatureUnit}</h6>
                 </div>
             </div>
         );
