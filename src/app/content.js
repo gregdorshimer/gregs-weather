852288776fcb9499d7fcf61eb8968182b9568cba
Greg Dorshimer <greg.dorshimer@protonmail.com>
2026-01-16 16:37:00 -0500

    re-factoring content functions to support future front end changes

--- src/app/content.js
+++ src/app/content.js
@@ -1,28 +1,81 @@
 import { useState } from "react";
 import axios from 'axios';
+import { getGeocode, getLatLng } from "use-places-autocomplete";
 import ForecastTile from "./forecast-tile";
 import Loc from "./loc.js";
 import SearchBar from "./search-bar.js";
 
 export default function Content() {
-    const [locs, setLocs] = useState([]);
+    const [cachedLocs, setCachedLocs] = useState([]);
+    const [currentLoc, setCurrentLoc] = useState(null);
     const [forecast, setForecast] = useState(null);
 
-    // function for construction the url that can get a forecast from NWS for a given gridpoint and office
-    function buildForecastURL(office, gridpoints) {
-        return "https://api.weather.gov/gridpoints/" + office + "/" + gridpoints + "/forecast";
-    }
+    // function for getting the office code and gridpoints from NWS for given coords:
+    const getOfficeGridpoints = async (coords) => {
+        const [lat, lng] = coords.split(",");
+        try {
+            const response = await axios.get(`https://api.weather.gov/points/${lat}%2C${lng}`);
+            const office = response.data.properties.gridId;
+            const gridpoints = `${response.data.properties.gridX},${response.data.properties.gridY}`;
+            return {office, gridpoints};
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    };
 
-    // function for getting a forecast, given an NWS office and gridpoints
-    const fetchForecast = (office, gridpoints) => {
-        let forecastURL = buildForecastURL(office, gridpoints);
-        axios.get(forecastURL)
-            .then(({data}) => {
-                setForecast(data);
-            })
-            .catch(error => {
-                console.log(error);
-            });
+    // function for getting a forecast for the given coords from NWS:
+    const getForecast = async (coords) => {
+        const obj = await getOfficeGridpoints(coords);
+        try {
+            const response = await axios.get(`https://api.weather.gov/gridpoints/${obj.office}/${obj.gridpoints}/forecast`);
+            return response.data;
+        } catch (error) {
+            console.error(error);
+            return error;
+        }
+    };
+
+    const selectNewLoc = async (option) => {
+        // "option" is the object that is returned by the Google Places API call
+        if (!option) return;
+
+        // create the newLoc object:
+        let newLoc = {
+            city: option.city,
+            state: option.state,
+        };
+
+        try {
+            // the results of 'usePlacesAutocomplete' hook do not contain coords, so have to fetch them here
+            // using the placeId, then set them on the newLoc object:
+            const results = await getGeocode({ placeId: option.value });
+            const { lat, lng } = await getLatLng(results[0]); // function to extract {lat, lng} from the complicated json object returned by getGeocode
+            newLoc.coords = `${lat},${lng}`;
+
+            // set the newly geocoded loc to be the currentLoc, get its forecast, and set the forecast:
+            setCurrentLoc(newLoc);
+            const newForecast = await getForecast(newLoc.coords);
+            setForecast(newForecast);
+
+            // add the newLoc to the cachedLocs:
+            const newCachedLocs = [...cachedLocs]; // spreading then re-assigning ensures a re-render when setCachedLocs is called
+            // let newCachedLocs = cachedLocs;   <-- i.e. this doesn't cause a re-render when setCachedLocs is called below
+            newCachedLocs.unshift(newLoc);
+            setCachedLocs(newCachedLocs.slice(0, process.env.NEXT_PUBLIC_LOC_CACHE_SIZE));
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const selectCachedLoc = async (loc) => {
+        try {
+            setCurrentLoc(loc);
+            const newForecast = await getForecast(loc.coords);
+            setForecast(newForecast);
+        } catch (error) {
+            console.error(error);
+        }
     };
 
     return (
@@ -30,20 +83,17 @@ export default function Content() {
             <div className="flex justify-center pb-4">
                 <div className="max-w-md w-full">
                     <SearchBar
-                        locs={locs}
-                        locSetter={setLocs}
-                        forecastFetcher={fetchForecast}
+                        selectNewLocHandler={selectNewLoc}
                     />
                 </div>
-
             </div>
 
             <div className="flex gap-2 overflow-x-auto pb-4">
-                {locs.map((loc, index) => (
+                {cachedLocs.map((loc, index) => (
                     <Loc
-                    key={`${index}-${loc.coords}`}
-                    info={loc}
-                    clickHandler={fetchForecast}
+                        key={`${index}-${loc.coords}`}
+                        info={loc}
+                        selectCachedLocHandler={selectCachedLoc}
                     />
                 ))}
             </div>
--- src/app/search-bar.js
+++ src/app/search-bar.js
@@ -1,9 +1,7 @@
-"use client";
 import AsyncSelect from 'react-select/async';
-import axios from 'axios';
-import usePlacesAutocomplete, { getGeocode, getLatLng } from "use-places-autocomplete";
+import usePlacesAutocomplete from "use-places-autocomplete";
 
-export default function SearchBar({locs, locSetter, forecastFetcher}) {
+export default function SearchBar({selectNewLoc}) {
     // define the hook for the suggested results of the search query:
     // here, status could be e.g. "OK" or "ZERO_RESULTS", while data is an array of suggestions returned by Google
     // and value is the contents of the text box
@@ -15,6 +13,7 @@ export default function SearchBar({locs, locSetter, forecastFetcher}) {
     });
 
     // define the function wrapper that will perform the google autocomplete search:
+    // inputValue is what is in the text field, and callback calls the onChange prop of <AsyncSelect> below 
     const loadOptions = (inputValue, callback) => {
         if (!ready || inputValue.length < 2) return callback([]);
 
@@ -38,58 +37,6 @@ export default function SearchBar({locs, locSetter, forecastFetcher}) {
         }
     };
 
-    const selectHandler = async (option) => {
-        // "option" is the object that is returned by the Google Places API call
-        if (!option) return;
-
-        const newLocs = [...locs]; // spreading then re-assigning ensures a re-render when locSetter is called
-        // let newLocs = locs;   <-- i.e. this doesn't cause a re-render when locSetter is called below
-
-        // create the newLoc object:
-        let newLoc = {
-            city: option.city,
-            state: option.state,
-        };
-
-        try {
-            // the results of 'usePlacesAutocomplete' hook do not contain coords, so have to fetch them here
-            // using the placeId, then set them on the newLoc object:
-            const results = await getGeocode({ placeId: option.value });
-            const { lat, lng } = await getLatLng(results[0]); // function to extract {lat, lng} from the complicated json object returned by getGeocode
-            newLoc.coords = `${lat},${lng}`;
-
-            // get the gridpoint info for the selected location's coordinates, and add to the newLoc object:
-            const gridpointURL = buildGridpointURL(newLoc.coords);
-            const response = await axios.get(gridpointURL);
-            const data = response.data;
-            newLoc.office = data.properties.gridId;
-            newLoc.gridpoints = `${data.properties.gridX},${data.properties.gridY}`;
-
-            // remove existing instance of the selected city from newLocs by finding the index of it, then 
-            // splicing out the item if it was present
-            const idx = newLocs.findIndex(
-                (l) => l.city == newLoc.city && l.state == newLoc.state
-            );
-            if (idx !== -1) {
-                newLocs.splice(idx, 1);
-            }
-
-            // add newLoc to the top of the list of newLocs, and assign to state:
-            newLocs.unshift(newLoc);
-            locSetter(newLocs.slice(0, process.env.NEXT_PUBLIC_LOC_CACHE_SIZE)); // passing a slice ensures that it is a new array reference, forcing a re-render
-            
-            // fetch forecast:
-            forecastFetcher(newLoc.office, newLoc.gridpoints);
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    function buildGridpointURL(coords) {
-        const [lat, lng] = coords.split(",");
-        return `https://api.weather.gov/points/${lat}%2C${lng}`;
-    }
-
     return (
         <>
             <AsyncSelect 
@@ -98,8 +45,9 @@ export default function SearchBar({locs, locSetter, forecastFetcher}) {
                 placeholder="Search US cities..."
                 // loadOptions is what happens on keystroke:
                 loadOptions={loadOptions} 
-                // onChange is what happens when an option from the dropdown is selected:
-                onChange={(option) => selectHandler(option)} />
+                // onChange is what happens when an option from the dropdown is selected. this is invoked
+                // in loadOptions using callback, and option is whatever is selected
+                onChange={(option) => selectNewLoc(option)} />
         </>
     );
 }
