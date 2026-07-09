568bf0f7a5a5754c4ae03d0dbe255f7f9f3a156f
Greg Dorshimer <greg.dorshimer@protonmail.com>
2026-07-09 15:35:55 -0400

    typo fix

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-// a hook for keeping a state variable synchronized with a local storage key
+// a hook for keeping a state variable synchronized with a local storage key/value
 export function useLocalStorage(key, defaultValue) {
   const [value, setValue] = useState(() => {
     const item = localStorage.getItem(key);
